3263fc98b55143ef948882b04579ba588e0e70ac
bestxxt <scauxxt@163.com>
2025-05-14 02:24:45 -0400

    fix: adjust alignment of buttons in Bottom component

--- src/components/Bottom.tsx
+++ src/components/Bottom.tsx
@@ -20,7 +20,7 @@ export default function Bottom({ loading, user, onPicture, onAdd, onAudio }: Bot
     }
 
     return (
-        <div className="fixed bottom-0 left-0 right-0 w-full bg-white border-t shadow z-50 flex items-center justify-evenly h-20">
+        <div className="fixed bottom-0 left-0 right-0 w-full bg-white border-t shadow z-50 flex items-start justify-evenly h-20">
             <button
                 className="flex flex-col items-center justify-center p-3 text-gray-700 hover:bg-gray-100 rounded-full transition "
                 onClick={onPicture}
