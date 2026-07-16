a18491294d01c9bbaa9eb0cfb678e4be5cc0e5a3
bestxxt <scauxxt@gmail.com>
2026-07-16 15:03:24 -0400

    Dockerize for direct deployment with nip.io

--- backend/src/prisma.ts
+++ backend/src/prisma.ts
@@ -1,5 +1,5 @@
 import { PrismaClient } from '@prisma/client';
 import { PrismaBetterSqlite3 } from '@prisma/adapter-better-sqlite3';
 
-const adapter = new PrismaBetterSqlite3({ url: './dev.db' });
+const adapter = new PrismaBetterSqlite3({ url: process.env.DATABASE_URL || './dev.db' });
 export const prisma = new PrismaClient({ adapter });
